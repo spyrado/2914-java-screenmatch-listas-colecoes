54ce8349d0f22bcf517e07d69b9de0350e2950bb
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2025-10-27 22:30:44 -0300

    feat: aprendendo mais sobre lista + cast + instanceof

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,36 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+
+  public static void main(String[] args) {
+    Serie lost = new Serie("Lost", 2000);
+    lost.avalia(6);
+    Filme meuFilme = new Filme("O poderoso chefão", 1970);
+    meuFilme.avalia(8);
+    Filme outroFilme = new Filme("Avatar", 2023);
+    outroFilme.avalia(9);
+    Filme filmeDoPaulo = new Filme("Dogville", 2003);
+    filmeDoPaulo.avalia(10);
+
+    ArrayList<Titulo> lista = new ArrayList<>();
+    lista.add(filmeDoPaulo);
+    lista.add(meuFilme);
+    lista.add(outroFilme);
+    lista.add(lost);
+
+    for (Titulo item: lista) {
+      System.out.println(item.getNome());
+      if(item instanceof Filme filme) {
+        System.out.println(filme.getClassificacao());
+      }
+    }
+
+//    System.out.println(lista);
+  }
+}
